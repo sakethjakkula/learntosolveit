66935d317a684df4c2aa3a261f8fda957ebee6ec
sakethjakkula <saketh.linuxs@gmail.com>
2018-02-16 11:34:08 +0530

    Update Ex_1.13_His_Horizontal.c
    This extension will give output in a more nice way. It also removes unnecessary railings given at the starting of the input.
    
    Input: i am the best
    Out put:
    i         =
    am        ==
    the       ===
    best      ====

--- languages/cprogs/Ex_1.13_His_Horizontal.c
+++ languages/cprogs/Ex_1.13_His_Horizontal.c
@@ -6,17 +6,33 @@
 
 #include<stdio.h>
 
-int main(void)
+#define SP 10
+int main()
 {
-	int c;
+int c,nc=0,i=0,stat=0;
 
-	while((c=getchar()) != EOF)
-	{
-		if( c == ' ' || c == '\n' || c == '\t')
-			putchar('\n');
-		else
-			putchar('*');
-	}
-	return 0;
+while((c = getchar()) != EOF)
+{
+if(c == ' ' || c == '\t' || c == '\n')
+{
+if(stat)
+{
+for(i=0;i<(SP-nc);i++)
+putchar(' ');
+for(;nc >0;--nc)
+printf("=");
+putchar('\n');
+stat =0;
 }
+}
+else
+{
+stat=1;
+putchar(c);
+nc++;
+}
+}
+return 0;
+}
+
 
